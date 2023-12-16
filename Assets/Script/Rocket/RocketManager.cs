43aaed04121f76eb6f70331d50938f572fd93f32
CuteDragonMeta <maxhugo.arbete@gmail.com>
2023-12-16 22:21:16 +0100

    Rocket Placement and Spawning

--- Assets/Script/Rocket/RocketManager.cs
+++ Assets/Script/Rocket/RocketManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RocketManegar : MonoBehaviour
 {
+    [SerializeField]GameObject CurrentRocket;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,14 @@ public class RocketManegar : MonoBehaviour
     {
         
     }
+
+    IEnumerator rocketSpawn(Collider collision, int time, GameObject Rocket){
+        yield return new WaitForSeconds(time);
+        Instantiate(Rocket, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider collision){
+        StartCoroutine(rocketSpawn(collision, 5, CurrentRocket));
+    }
+
 }
